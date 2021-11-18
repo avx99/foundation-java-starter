f160ee3f1b927cd7db46acffb912c66dcba3ad0d
Titus Gahissy <titus@gahissy.com>
2021-11-18 00:17:10 +0000

    Initial commit

--- app-service/src/main/java/foundation/app/Application.java
+++ app-service/src/main/java/foundation/app/Application.java
@@ -5,10 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import static foundation.app.Application.APP_PKG;
+import static foundation.app.Application.FOUNDATION_PKG;
+
 @SpringBootApplication
-@EntityScan(basePackages = {"io.soffa.foundation", "foundation.app"})
-@EnableJpaRepositories(basePackages = {"io.soffa.foundation", "foundation.app"})
+@EntityScan(basePackages = {FOUNDATION_PKG, APP_PKG})
+@EnableJpaRepositories(basePackages = {FOUNDATION_PKG, APP_PKG})
 public class Application {
+
+    public static final String FOUNDATION_PKG = "io.soffa.foundation";
+    public static final String APP_PKG = "foundation.app";
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
