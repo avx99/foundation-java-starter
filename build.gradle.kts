5a34b3e71445fbb3df904b632f209a8b07015d60
Titus <titus@gahissy.com>
2021-11-26 14:34:33 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("io.soffa.gradle:soffa-gradle-plugin:2.1.2")
+        classpath("io.soffa.gradle:soffa-gradle-plugin:2.1.8")
     }
 }
 
