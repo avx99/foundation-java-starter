3dcbc8cb8b5aecf494a30aff7509211582896101
Titus Gahissy <titus@gahissy.com>
2022-03-14 22:17:45 +0000

    Document Echo resource

--- app-api/src/main/java/foundation/app/api/resource/EchoResource.java
+++ app-api/src/main/java/foundation/app/api/resource/EchoResource.java
@@ -2,15 +2,16 @@ package foundation.app.api.resource;
 
 import dev.soffa.foundation.context.Context;
 import dev.soffa.foundation.resource.Resource;
+import foundation.app.api.schema.EchoInput;
 import foundation.app.api.schema.Message;
 import foundation.app.api.usecase.Echo;
-import foundation.app.api.schema.EchoInput;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.validation.Valid;
 
 @Tags(
@@ -19,6 +20,13 @@ import javax.validation.Valid;
 @RestController // Cette interface sera implémentée automatiquement lorsque cette annotation est présente + Resource
 public interface EchoResource extends Resource {
 
+
+    /**
+     * Commentaire développeur pour la méthode echo
+     *
+     * @param input   Données utilisateur
+     * @param context Injectée automatiquement avec les informations de la requête en cours (n'apparaît pas dans la documentation OpenAPI)
+     */
     @Operation(
         method = "POST",
         summary = "Echo the input message"
