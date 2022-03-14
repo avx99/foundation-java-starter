65ae7284a94d5c597f7b8360265b124deb0ee6e2
Titus Gahissy <titus@gahissy.com>
2022-03-14 22:18:15 +0000

    Document Echo resource

--- app-service/src/main/java/foundation/app/Bootstrap.java
+++ app-service/src/main/java/foundation/app/Bootstrap.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Bootstrap {
 
     public static void main(String[] args) {
+        // Remarquez l'ulisation de Foundation en lieu et place de la classe SpringBoot
         Foundation.run(Bootstrap.class, args);
     }
 
